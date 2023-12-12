ae5357e369e91410adb08fdc4611cebc0c8fd4c6
Frederic Guyont <you@example.com>
2023-12-12 15:10:09 +0100

    ADD: ModelMapper

--- src/main/java/com/openclassrooms/chatopapi/ChatopapiApplication.java
+++ src/main/java/com/openclassrooms/chatopapi/ChatopapiApplication.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.chatopapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatopapiApplication {
@@ -9,5 +11,9 @@ public class ChatopapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatopapiApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
