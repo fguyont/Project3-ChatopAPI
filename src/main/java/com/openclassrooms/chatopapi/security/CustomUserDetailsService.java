2f6ae97777f5defcc0116ddb5f5942846768e741
Frederic Guyont <you@example.com>
2023-12-12 15:11:25 +0100

    ADD: JWT with Spring Security

---
+++ src/main/java/com/openclassrooms/chatopapi/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.chatopapi.security;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.openclassrooms.chatopapi.model.User;
+import com.openclassrooms.chatopapi.repository.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
+        
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authorities);
+    }
+}
