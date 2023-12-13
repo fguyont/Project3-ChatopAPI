88d4449bf535fa9b8ecd75cd12a5de184c744369
Frederic Guyont <you@example.com>
2023-12-13 15:40:34 +0100

    ADD: Swagger documentation test

--- src/main/java/com/openclassrooms/chatopapi/controller/RentalsController.java
+++ src/main/java/com/openclassrooms/chatopapi/controller/RentalsController.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/rentals")
 public class RentalsController {
 
 	@GetMapping("")
+	@Operation(summary = "Gets all rentals")
 	public String getResource() {
 		return "a value...";
 	}
