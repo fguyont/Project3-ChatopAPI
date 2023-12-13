2632399ceea2633da16153e72be2fd5e097c597a
Frederic Guyont <you@example.com>
2023-12-13 15:44:43 +0100

    ADD: Constructors

--- src/main/java/com/openclassrooms/chatopapi/dto/LoginRequest.java
+++ src/main/java/com/openclassrooms/chatopapi/dto/LoginRequest.java
@@ -6,4 +6,14 @@ import lombok.Data;
 public class LoginRequest {
 	private String email;
 	private String password;
+	
+	public LoginRequest() {
+		super();
+	}	
+	
+	public LoginRequest(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
 }
