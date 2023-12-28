edd8c0e6b5626bc26bdb28fb2d0447cc9518e894
Frederic Guyont <you@example.com>
2023-12-28 15:50:40 +0100

    DEL: Unused imports

--- src/main/java/com/openclassrooms/chatopapi/security/CustomUserDetailsService.java
+++ src/main/java/com/openclassrooms/chatopapi/security/CustomUserDetailsService.java
@@ -3,15 +3,12 @@ package com.openclassrooms.chatopapi.security;
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 import com.openclassrooms.chatopapi.model.User;
 import com.openclassrooms.chatopapi.repository.UserRepository;
 
@@ -21,20 +18,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
     private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-
-        User user = userRepository.findByEmail(usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User does not exist by Username or Email"));
-        
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-
-		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-
-        return new org.springframework.security.core.userdetails.User(
-                usernameOrEmail,
-                user.getPassword(),
-                authorities);
-    }
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User does not exist by Username or Email"));;
+		if(user == null) {
+			throw new UsernameNotFoundException("User not found", null);
+		}
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+	}
 }
