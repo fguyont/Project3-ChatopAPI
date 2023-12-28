f879442671bd06ea94a4209432057e620136cdc4
Frederic Guyont <you@example.com>
2023-12-28 15:55:48 +0100

    ADD: File management

---
+++ src/main/java/com/openclassrooms/chatopapi/service/FileUpload.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.chatopapi.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+public interface FileUpload {
+    String uploadFile(MultipartFile multipartFile) throws IOException;
+}
