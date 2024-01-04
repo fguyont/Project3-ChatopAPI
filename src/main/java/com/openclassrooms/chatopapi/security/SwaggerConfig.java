5532c8aca78f4074ef2bb69f76baf88614cf64b6
Frederic Guyont <you@example.com>
2024-01-04 15:00:52 +0100

    ADD: SwaggerConfig for auth in Swagger

---
+++ src/main/java/com/openclassrooms/chatopapi/security/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.chatopapi.security;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for Swagger documentation.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Configures the OpenAPI settings for Swagger.
+     *
+     * @return An instance of {@link OpenAPI} containing the custom configuration.
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearer-key",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
